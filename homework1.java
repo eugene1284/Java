42dbc39bb9a21a9e2b38db57e2435963e71db919
Eugene <jenksbu@gmail.com>
2022-09-06 18:19:42 +0300

    edit homework1

--- homework1.java
+++ homework1.java
@@ -21,7 +21,7 @@ import java.util.Scanner;
 
 public class homework1 {
     public static void main(String[] args) {
-        Scanner iScanner = new Scanner(System.in);
+        Scanner iScanner = new Scanner(System.in, "Cp866");
         do {
             calc(iScanner);
         } while (question(iScanner));
@@ -58,6 +58,6 @@ public class homework1 {
         System.out.printf("\nХотите продолжить?\n");
         byte[] b = scan.next().getBytes();
         String answer = new String(b);
-        return answer.equalsIgnoreCase("DA");
+        return answer.equalsIgnoreCase("да");
     }
 }
