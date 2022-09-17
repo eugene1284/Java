564f37558d360f0b1b43f1027afd50da332156d5
Eugene <jenksbu@gmail.com>
2022-09-17 15:30:25 +0300

    added hw3_3

---
+++ homework3_3.java
@@ -0,0 +1,40 @@
+/*
+Задан целочисленный список ArrayList. Найти 
+1. минимальное, 
+2. максимальное, 
+3. среднее из этого списка (просуммировать все элементы и поделить / среднеарифметическое).
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class homework3_3 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(5);
+        System.out.println("лист: " + list);
+
+        int min = list.get(0);
+        int max = list.get(0);
+        int sum = 0;
+
+        for (Integer i : list) {
+            sum += i;
+            if (i < min)
+                min = i;
+            if (i > max)
+                max = i;
+        }
+        double avg = (double) sum / (double) list.size();
+
+        System.out.println("минимальное число: " + min);
+        System.out.println("максимальное число: " + max);
+        System.out.println("Среднеарифметическое заданного списка: " + avg);
+
+    }
+
+}
